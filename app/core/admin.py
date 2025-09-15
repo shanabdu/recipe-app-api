7eb963aaca7477eb6ebd145553d9ac96226f436c
Abdul Shukkoor Shanavas <abdul.shanavas@dge.gov.ae>
2025-09-15 22:50:02 +0400

    updated with the commands

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
-
-# Register your models here.
+# ...existing code...
+# Remove: from django.db import models
+# ...existing code...
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,3 @@
-from django.db import models
-
-# Create your models here.
+# ...existing code...
+# Remove: from django.db import models
+# ...existing code...
